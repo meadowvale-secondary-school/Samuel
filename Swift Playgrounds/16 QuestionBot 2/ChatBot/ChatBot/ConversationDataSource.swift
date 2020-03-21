4fbe6ac94aaea796aa94eae69bf73a7c538ec39d
Test <samueljojoquansah@gmail.com>
2020-03-20 23:35:14 -0400

    Completion of this lesson.

--- Swift Playgrounds/16 QuestionBot 2/ChatBot/ChatBot/ConversationDataSource.swift
+++ Swift Playgrounds/16 QuestionBot 2/ChatBot/ChatBot/ConversationDataSource.swift
@@ -3,20 +3,36 @@ class ConversationDataSource {
     
     /// The number of Messages in the conversation
     var messageCount = 0
+    var messages = [Message]()
+    
     
     /// Add a new question to the conversation
     func add(question: String) {
         print("Asked to add question: \(question)")
+        let message = Message(date: Date(), text: question, type: .question)
+        messages.append(message)
     }
     
     /// Add a new answer to the conversation
     func add(answer: String) {
         print("Asked to add answer: \(answer)")
+        let message = Message(date: Date(), text: answer, type: .answer)
+        var messageCount: Int{
+            return messages.count
+        }
+        var messages = [openingLine]
+        
     }
     
     /// The Message at a specific point in the conversation
     func messageAt(index: Int) -> Message {
         print("Asking for message at index \(index)")
-        return Message(date: Date(), text: "Hello, world!", type: .answer)
+        return messages[index]
+        //if index % 2 == 0{
+        //    return Message(date: Date(), text: "Question \(index / 2)", type: .question)
+        //}
+        //else{
+        //    return Message(date: Date(), text: "Answer \(index / 2)", type: .answer)
+        //}
     }
 }
