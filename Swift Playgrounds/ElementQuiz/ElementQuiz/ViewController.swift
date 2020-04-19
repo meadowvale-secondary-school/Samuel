0fcb6300b59af58c9a1f3cc6f7e144d66133c138
Test <samueljojoquansah@gmail.com>
2020-04-18 21:03:56 -0400

    finished lesson 18.2

--- Swift Playgrounds/ElementQuiz/ElementQuiz/ViewController.swift
+++ Swift Playgrounds/ElementQuiz/ElementQuiz/ViewController.swift
@@ -12,17 +12,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateElement()
     }
 
-    @IBOutlet weak var imageView: UIView!
+    @IBOutlet weak var UIImageView: UIView!
     
     @IBOutlet weak var answerLabel: UILabel!
     
     @IBAction func gotoNextElement(_ sender: Any) {
+        currentElementIndex += 1
+        if currentElementIndex >= elementList.count{
+            currentElementIndex = 0
+        }
+        updateElement()
     }
     
     
     
+    @IBAction func showAnswer(_ sender: Any) {
+        answerLabel.text = elementList[currentElementIndex]
+    }
+    let elementList = ["Carbon", "Gold", "Chlorine", "Sodium"]
+    
+    var currentElementIndex = 0
+    
+    func updateElement()
+    {
+        answerLabel.text = "?"
+        let elementName = elementList[currentElementIndex]
+        let image = UIImage(named: elementName)
+        //imageView.image = image
+    }
 }
 
