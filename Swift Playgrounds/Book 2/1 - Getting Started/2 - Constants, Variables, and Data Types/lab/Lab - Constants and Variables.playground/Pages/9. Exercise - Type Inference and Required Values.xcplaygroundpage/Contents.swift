d71bac38716a986c24f8d1753d6a5166c147a4a8
Test <samueljojoquansah@gmail.com>
2020-06-22 23:13:57 -0400

    Dump of all work 1

--- Swift Playgrounds/Book 2/1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/9. Exercise - Type Inference and Required Values.xcplaygroundpage/Contents.swift
+++ Swift Playgrounds/Book 2/1 - Getting Started/2 - Constants, Variables, and Data Types/lab/Lab - Constants and Variables.playground/Pages/9. Exercise - Type Inference and Required Values.xcplaygroundpage/Contents.swift
@@ -8,16 +8,17 @@ print("No because it complains of no type annotation")
 /*:
  Now assign a value to `name`, and print it to the console.
  */
-
+var name = "Samuel"
+print(name)
 
 /*:
  Declare a variable called `distanceTraveled` and set it to 0. Do not give it an explicit type.
  */
-
+var distanceTraveled = Double(0)
 
 /*:
  Now assign a value of 54.3 to `distanceTraveled`. Does the code compile? Go back and set an explicit type on `distanceTraveled` so the code will compile.
  */
-
+distanceTraveled = 54.3
 
 //: [Previous](@previous)  |  page 9 of 10  |  [Next: App Exercise - Percent Completed](@next)
