131ce751571dc90f820dd04d8a059674485066ef
Test <samueljojoquansah@gmail.com>
2020-06-23 08:30:52 -0400

    Dump of all work 2

--- Swift Playgrounds/Book 2/1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/1. Exercise - Basic Arithmetic.xcplaygroundpage/Contents.swift
+++ Swift Playgrounds/Book 2/1 - Getting Started/3 - Operators/lab/Lab - Operators.playground/Pages/1. Exercise - Basic Arithmetic.xcplaygroundpage/Contents.swift
@@ -3,27 +3,34 @@
  
  You decide to build a shed and want to know beforehand the area of your yard that it will take up. Create two constants, `width` and `height`, with values of 10 and 20, respectively. Create an `area` constant that is the result of multiplying the two previous constants together, and print out the result.
  */
- 
+var width = 10
+var height = 20
+var area = width * height
 
 /*:
  You decide that you'll divide your shed into two rooms. You want to know if dividing it equally will leave enough room for some of your larger storage items. Create a `roomArea` constant that is the result of dividing `area` in half. Print out the result.
  */
-
+var roomArea = area/2
 
 /*:
  Create a `perimeter` constant whose value equals `width` plus `width` plus `height` plus `height`, then print out the result.
  */
-
+var perimeter = width + width + height + height
+print(perimeter)
 
 /*:
  Print what you would expect the result of integer division of 10 divided by 3 to be. Create a constant, `integerDivisionResult` that is the result of 10 divided by 3, and print the value.
  */
-
+var integerDivisionResult = 10/3
+print(integerDivisionResult)
 
 /*:
  Now create two constants, `double10` and `double3`, set to 10 and 3, and declare their types as `Double` values. Declare a final constant `divisionResult` equal to the result of `double10` divided by `double3`. Print the value of `divisionResult`. How does this differ from the value when using integer division?
  */
-
+var double10 = Double(10)
+var double3 = Double(3)
+var divisionResult = double10/double3
+print(divisionResult)
 
 /*:
  Given the value pi (3.1415927), create a `radius` constant with a value of 5.0, then calculate the diameter and circumference of the circle using the following equations, and print the results:
@@ -32,7 +39,13 @@
  
  *circumference = 2 * pi * radius.*
  */
-let pi = 3.1415927 
+let pi = 3.1415927
+let radius = 5.0
+var diameter = 2 * radius
+var circumference = 2 * pi * radius
+print(circumference)
+print(diameter)
+
 
 
 //: page 1 of 8  |  [Next: App Exercise - Fitness Calculations](@next)
